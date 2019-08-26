1db90af0faa3bc6f60c3def4622902185128beb2
rahul2312jsr <rahul2312jsr@gmail.com>
2019-08-26 12:08:33 -0700

    Add files via upload

---
+++ main/MLTester.java
@@ -0,0 +1,16 @@
+package com.machinelearning.main;
+
+public class MLTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			LogisticRegressionDemo.process();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
